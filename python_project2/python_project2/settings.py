738eeeed8798ca4ca92b499c4ea28659d7a2776f
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-04-21 00:40:35 -0400

    they forgot to put the admin in the settings.py of the main, which is suppose to be in the installed apps

--- python_project2/python_project2/settings.py
+++ python_project2/python_project2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin.apps.AdminConfig',
     'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
