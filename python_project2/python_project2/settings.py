5cad9857c5cef92d82d228fcd2cd11cd3df75276
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-04-21 16:17:22 -0400

    just added a filed for the products in the drawio

--- python_project2/python_project2/settings.py
+++ python_project2/python_project2/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'python_project2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
