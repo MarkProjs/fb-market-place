d69fdbcbc4c5e9f2b61441a9a1f06bb4d7aaf0d8
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-04-24 16:53:25 -0400

    adding the django-crispy library

--- python_project2/python_project2/settings.py
+++ python_project2/python_project2/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user_management.apps.UserManagementConfig',
+    'crispy_forms',
     'webadminapp.apps.WebadminappConfig',
     'store.apps.StoreConfig',
     'django.contrib.admin',
@@ -132,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
