83c7a022844341eda16522983bebbbdebce667ec
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-04-27 00:13:52 -0400

    putting a login and log out view

--- python_project2/user_management/views.py
+++ python_project2/user_management/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
-def login(request):
-    return render(request, 'user_management/login.html', {'title': 'user_management-Login Page'})
-
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('store-home')
+            messages.success(request, f'Account has been created! You can now log in!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user_management/register.html', {'form': form})
 
-# message.debug
-# message.info
-# message.success
-# message.warning
-# message.error
+
+@login_required
+def profile(request):
+    return render(request, 'user_management/profile.html')
