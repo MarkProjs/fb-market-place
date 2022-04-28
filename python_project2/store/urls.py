f7c3e04e569c8a71c5165945d9496542caceba23
нур нур <aminaturdalieva@gmail.com>
2022-04-28 13:58:15 -0400

    adding detail and create view to the products

--- python_project2/store/urls.py
+++ python_project2/store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import ProductListView, ProductDetailView, ProductCreateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='store-home'),
+    path('', ProductListView.as_view(), name='store-home'),
+    path('product/<pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('product/new/', ProductCreateView.as_view(), name='product-create'),
     path('about/', views.about, name='store-about'),
 ]
 
--- python_project2/store/views.py
+++ python_project2/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import ListView, DetailView, CreateView
 # Create your views here.
 
 
@@ -8,6 +9,21 @@ def home(request):
         'products': Product.objects.all()
     }
     return render(request, 'store/home.html', context)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'store/home.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ['name', 'description']
 
 
 def about(request):
