03993d58af9f7f92778d8c088d03c97ec8de47dd
Mark Agluba <markagluba@Marks-MacBook-Pro.local>
2022-05-03 15:48:25 -0400

    initialzing the web messaging app

--- python_project2/web_messaging/models.py
+++ python_project2/web_messaging/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name="sender")
+    receiver = models.ManyToManyField(User, related_name="receiver")
+    msg_body=models.CharField(max_length=1000)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    unread=models.BooleanField(default = True)
+
