56c9ccde21858f7d22c5780243cb57201e8a565e
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-05-07 10:09:18 -0400

    fixed the problem where the one on top of the inbox putting the db

--- python_project2/web_messaging/urls.py
+++ python_project2/web_messaging/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('send/', views.MessageView.as_view(), name='web_messaging_send'),
+    path('inbox/', views.inbox, name='web_messaging_inbox'),
 ]
--- python_project2/web_messaging/views.py
+++ python_project2/web_messaging/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.models import User
 from .models import Message
 from .forms import SendMessage
@@ -25,8 +25,16 @@ class MessageView(CreateView):
             msg_body = msg_form.cleaned_data.get('message')
             Message.objects.create(sender=request.user, receiver=receiver, message=msg_body)
             messages.success(request, f'Message sent to {receiver}')
-            return redirect()
         return render(request, self.template_name, {'msg_form': msg_form})
 
+
+def inbox(request):
+    context = {
+        'view_message': Message.objects.all().filter(receiver__username__contains=request.user)
+    }
+    return render(request, 'web_messaging/inbox.html', context)
+# class MessageListView(ListView):
+#     model = Message
+#     template_name = 'web_messaging/inbox.html'
 
 
