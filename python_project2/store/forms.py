a6b8a422d2ab29081e75d53c29604e2375e7d005
Jacky Tat <tatjacky123@gmail.com>
2022-05-09 18:20:12 -0400

    ratings done

--- python_project2/store/forms.py
+++ python_project2/store/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from .models import Product, Comment
+from .models import Product, Comment, rating_choices
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('name', 'body')
+        fields = ('name', 'body', 'rating')
 
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'})
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 
