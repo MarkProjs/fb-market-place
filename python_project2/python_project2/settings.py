cabb9b594f8ee5b02390048cca56a3e979b54de6
Jacky Tat <tatjacky123@gmail.com>
2022-05-14 18:10:42 -0400

    reset password works

--- python_project2/python_project2/settings.py
+++ python_project2/python_project2/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('postgrespython')
-EMAIL_HOST_PASSWORD = os.environ.get('palcltnovwcsdcuy')
+EMAIL_HOST_USER = 'postgrespython'
+EMAIL_HOST_PASSWORD = 'palcltnovwcsdcuy'
 
 django_heroku.settings(locals())
