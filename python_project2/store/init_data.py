7ae5dffcea3144bc082f32212d107a772d118d77
Jacky Tat <tatjacky123@gmail.com>
2022-05-15 18:18:39 -0400

    Init Data

--- python_project2/store/init_data.py
+++ python_project2/store/init_data.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
-from .models import Product
+from django.db.models import Avg
+from .models import Product, Comment
 import random as rnd
 
 
@@ -47,7 +48,7 @@ product_lst = [
         'description': 'Limited edition Monkey Slippers',
         'size': 'Large',
         'image': 'default_product_img/monkeyslipper.jpg',
-        'address': '6940 Monkey Street',
+        'address': '2940 Avenue Pepega',
         'status': 'New'
     },
     {
@@ -127,10 +128,126 @@ product_lst = [
         'address': '133 Nowhere Street',
         'status': 'New'
     },
+    {
+        'owner': 'Jeremy',
+        'type': 'Shoes',
+        'name': 'Left Blue Slippers',
+        'price': 4,
+        'description': 'Left Blue Slippers, Lost the other one...',
+        'size': 'Medium',
+        'image': 'default_product_img/leftblueslipper.jpg',
+        'address': '420 Somewhere Street',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Amina',
+        'type': 'T-shirt',
+        'name': 'Hiking t-shirt',
+        'price': 50,
+        'description': 'Made of soft organic cotton',
+        'size': 'Medium',
+        'image': 'default_product_img/hikingshirt.jpg',
+        'address': '850 Lemon Street',
+        'status': 'New'
+    },
+    {
+        'owner': 'Jeremy',
+        'type': 'Accessories',
+        'name': 'Used Mask',
+        'price': 2,
+        'description': 'Mask that I used to go to school, dont worry I am triple vaxxed... You wont catch covid',
+        'size': '',
+        'image': 'default_product_img/usedmask.jpg',
+        'address': '250 Covid Street',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Charlie',
+        'type': 'Electronics',
+        'name': 'Old Alarm Clock',
+        'price': 10,
+        'description': 'Old school Alarm Clock',
+        'size': '',
+        'image': 'default_product_img/alarmclock.jpg',
+        'address': '332 Nowhere Street',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Charlie',
+        'type': 'Books',
+        'name': 'Cal-1 Manual',
+        'price': 39,
+        'description': 'Calculus 1 Manual, no notes written on it',
+        'size': '',
+        'image': 'default_product_img/cal1manual.jpg',
+        'address': '332 Nowhere Street',
+        'status': 'Refurbished'
+    },
+    {
+        'owner': 'Denis',
+        'type': 'Utilities',
+        'name': 'Used Deodorant',
+        'price': 9,
+        'description': 'My old deodorant, I got myself some Old Spice, so Im selling this one',
+        'size': '',
+        'image': 'default_product_img/deodorant.jpg',
+        'address': '552 IDontKnow Avenue',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Emma',
+        'type': 'Electronics',
+        'name': '88-keys Keyboard',
+        'price': 499,
+        'description': 'Model Alesis, used but good as new.',
+        'size': 'Medium',
+        'image': 'default_product_img/piano.jpg',
+        'address': '5322 Avenue Bonk',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Emma',
+        'type': 'Electronics',
+        'name': 'Used Nintendo Switch',
+        'price': 249,
+        'description': 'Nintendo Switch, conditions are good as new.',
+        'size': '',
+        'image': 'default_product_img/nintendoswitch.jpg',
+        'address': '5322 Avenue Bonk',
+        'status': 'Used'
+    },
+    {
+        'owner': 'Amina',
+        'type': 'Electronics',
+        'name': 'PS4',
+        'price': 299,
+        'description': 'Used PS4',
+        'size': '',
+        'image': 'default_product_img/ps4.jpg',
+        'address': '850 Lemon Street',
+        'status': 'New'
+    },
+    {
+        'owner': 'Frank',
+        'type': 'Electronics',
+        'name': 'Piano Pedal',
+        'price': 19,
+        'description': 'Used piano pedal, works for any kind of electronic keyboards',
+        'size': '',
+        'image': 'default_product_img/pianopedal.jpg',
+        'address': '2940 Avenue Pepega',
+        'status': 'Used'
+    },
 ]
 
-
 group_member = Group.objects.get(name="Members")
+obj_lst = []
+
+
+def update_ratings(prod):
+    avg_rating = Comment.objects.filter(product=prod).aggregate(Avg('rating'))
+    prod.rate = avg_rating['rating__avg']
+    prod.save()
 
 
 def init_data():
@@ -151,3 +268,72 @@ def init_data():
             address=product['address'],
             status=product['status']
         )
+
+        obj_lst.append(product)
+
+    obj_lst[0].likes.add(User.objects.get(username='Frank'))
+    obj_lst[0].likes.add(User.objects.get(username='Emma'))
+    obj_lst[0].likes.add(User.objects.get(username='Amina'))
+    obj_lst[0].likes.add(User.objects.get(username='Mark'))
+    obj_lst[0].likes.add(User.objects.get(username='Jeremy'))
+
+    Comment.objects.create(
+        product=obj_lst[0],
+        name=User.objects.get(username='Frank'),
+        body='Thats a nice laptop, what are the specs ?',
+        rating=3
+    )
+
+    Comment.objects.create(
+        product=obj_lst[0],
+        name=User.objects.get(username='Emma'),
+        body='Very nicee',
+        rating=4
+    )
+
+    update_ratings(obj_lst[0])
+
+    obj_lst[1].likes.add(User.objects.get(username='Jeremy'))
+    obj_lst[1].likes.add(User.objects.get(username='Mark'))
+    obj_lst[1].likes.add(User.objects.get(username='Denis'))
+
+    Comment.objects.create(
+        product=obj_lst[1],
+        name=User.objects.get(username='Denis'),
+        body='Thats a cool shirt',
+        rating=4
+    )
+
+    Comment.objects.create(
+        product=obj_lst[1],
+        name=User.objects.get(username='Mark'),
+        body='I need this',
+        rating=5
+    )
+
+    update_ratings(obj_lst[1])
+
+    obj_lst[3].likes.add(User.objects.get(username='Jeremy'))
+    obj_lst[3].flags.add(User.objects.get(username='Manny'))
+    obj_lst[3].flags.add(User.objects.get(username='Frank'))
+    obj_lst[3].flags.add(User.objects.get(username='Emma'))
+
+    Comment.objects.create(
+        product=obj_lst[3],
+        name=User.objects.get(username='Jeremy'),
+        body='Id like to order all your stocks bro',
+        rating=5
+    )
+
+    Comment.objects.create(
+        product=obj_lst[3],
+        name=User.objects.get(username='Manny'),
+        body='What in the worldd is this',
+        rating=1
+    )
+
+    update_ratings(obj_lst[3])
+
+
+
+
